ebb395e63be204dac2ab62b4a30b763ebf3d16e2
Will <ws.will09@gmail.com>
2026-05-22 23:45:28 +0100

    Added math unit conversion functions & input utility functions

--- inc/MEGEngine/Math.h
+++ inc/MEGEngine/Math.h
@@ -7,4 +7,10 @@
 #include "MEGEngine/math/vec2.h"
 #include "MEGEngine/math/quat.h"
 
+// free floating functions that don't belong to any class
+float ENGINE_API radians(float deg);
+float ENGINE_API degrees(float rad);
+
+float ENGINE_API clamp(float value, float min, float max);
+
 #endif //MEGENGINEPROJECT_MATH_H
--- inc/MEGEngine/input.h
+++ inc/MEGEngine/input.h
@@ -11,5 +11,34 @@
 #include "MEGEngine/input/raw_input_event_bus.h"
 #include "MEGEngine/input/player_action_bus.h"
 #include "MEGEngine/input/input_receiver.h"
+#include "MEGEngine/window.h"
+
+#include "MEGEngine/common.h"
+
+enum class InputMode {
+    CURSOR,
+    STICKY_KEYS,
+    STICKY_MOUSE_BUTTONS,
+    LOCK_KEY_MODS,
+    RAW_MOUSE_MOTION
+};
+
+enum class InputModeValue {
+    CURSOR_NORMAL,
+    CURSOR_HIDDEN,
+    CURSOR_DISABLED,
+    CURSOR_CAPTURED,
+    FALSE,
+    TRUE,
+    VOID
+};
+
+void ENGINE_API setCursorPosition(Window& window, float x, float y);
+void ENGINE_API setCursorPosition(Window& window, Vec2 pos);
+
+Vec2 ENGINE_API getCursorPosition(Window& window, Vec2* pos);
+
+void ENGINE_API setInputMode(Window& window, InputMode mode, InputModeValue value);
+InputModeValue ENGINE_API getInputMode(Window& window, InputMode mode);
 
 #endif //MEGENGINEPROJECT_INPUT_H
