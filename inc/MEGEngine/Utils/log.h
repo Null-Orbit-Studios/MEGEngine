6cb207dfa5869c2dab464dd9db1616008c43fb42
Will <ws.will09@gmail.com>
2026-06-03 01:07:27 +0100

    New version of logger

--- inc/MEGEngine/Utils/log.h
+++ inc/MEGEngine/Utils/log.h
@@ -5,6 +5,14 @@
 
 #include "MEGEngine/common.h"
 
+#ifndef __FUNCTION_NAME__
+    #ifdef _WIN32
+        #define __FUNC_NAME__ __FUNCTION__
+    #else
+        #define __FUNC_NAME__ __func__
+    #endif
+#endif
+
 enum class ENGINE_API LogLevel {
     DBG,
     INF,
@@ -12,8 +20,28 @@ enum class ENGINE_API LogLevel {
     ERR
 };
 
-
+/** 
+ * DEPRECATED - use LOG_DBG, LOG_INF, LOG_WRN, LOG_ERR macros instead
+ * i.e. LOG_DBG("Message %i", 1);
+ */
 void ENGINE_API Log(LogLevel level, const std::string& format, ...);
+
+class ENGINE_API Logger {
+public:
+    Logger() {}
+    ~Logger() {}
+
+    static void log(LogLevel level, const char* filepath, int line, const std::string& function, const std::string& format, ...);
+
+private:
+    static const char* get_file_basename(const std::string& filename);
+    static const std::string get_log_level_str(LogLevel level);
+};
+
+#define LOG_DBG(format, ...) Logger::log(LogLevel::DBG, __FILE__, __LINE__, __FUNC_NAME__, format, __VA_ARGS__)
+#define LOG_INF(format, ...) Logger::log(LogLevel::INF, __FILE__, __LINE__, __FUNC_NAME__, format, __VA_ARGS__)
+#define LOG_WRN(format, ...) Logger::log(LogLevel::WRN, __FILE__, __LINE__, __FUNC_NAME__, format, __VA_ARGS__)
+#define LOG_ERR(format, ...) Logger::log(LogLevel::ERR, __FILE__, __LINE__, __FUNC_NAME__, format, __VA_ARGS__)
 
 
 #endif //MEGENGINEPROJECT_LOG_H
